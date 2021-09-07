ebadeefaa2d5423b922d7dc23718de828b431bf9
49EHyeon42 <49EHyeon42@gmail.com>
2021-09-07 15:36:54 +0900

    Add template

---
+++ template/template.py
@@ -0,0 +1,15 @@
+import unittest
+
+def solution():
+    return
+
+class SolutionTest(unittest.TestCase):
+    def test_Method1(self):
+        return
+
+    def test_Method2(self):
+        return
+
+if __name__ == '__main__':
+    # Obey the Python coding convention provided in PEP8
+    SolutionTest.main()
