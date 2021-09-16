5898d092c856f5a7679d32888103096a3f29d920
49EHyeon42 <49EHyeon42@gmail.com>
2021-09-16 14:43:51 +0900

    Update 12930
    공백 이슈 관련 12930.md 내용 추가
    공백 이슈 과련 main.py 수정
    공백 이슈 관련 question.py 의 __createSpace 함수 추가

--- 12930/main.py
+++ 12930/main.py
@@ -2,22 +2,14 @@ import question
 import solution
 
 if __name__=="__main__":
-    parameter = question.Question(10, 10).createString()
+    parameter = question.Question(10, 3, 10).createString()
     
-    print("1. Input :", parameter)
+    print("Input :", parameter)
     if solution.mySolution(parameter) != solution.referenceSolution1(parameter):
-        print("error 1")
+        print("Output : error 1")
     elif solution.mySolution(parameter) != solution.referenceSolution2(parameter):
-        print("error 2")
+        print("Output : error 2")
     elif solution.mySolution(parameter) != solution.referenceSolution3(parameter):
-        print("error 3")
+        print("Output : error 3")
     else:
-        print("pass")
-
-    test = "Test C  oDe"
-    print("2. Input :", test)
-    if solution.mySolution(test) != solution.referenceSolution2(test):
-        print("1 :", solution.mySolution(test))
-        print("2 :", solution.referenceSolution2(test))
-    else:
-        print("pass")
+        print("Output : pass")
--- 12930/question.py
+++ 12930/question.py
@@ -5,9 +5,10 @@ import random
 # * 프로그래머스 문제
 class Question:
     # Constructor
-    # TODO 단어의 최대 길이와 문자열의 최대 길이 넣기
-    def __init__(self, max_word_length, max_string_length):
+    # TODO 단어의 최대 길이, 공백의 최대 길이, 문자열의 최대 길이 넣기
+    def __init__(self, max_word_length, max_space_length, max_string_length):
         self.__MAX_WORD_LENGTH = max_word_length
+        self.__MAX_SPACE_LENGTH = max_space_length
         self.__MAX_STRING_LENGTH = max_string_length
 
     # createWord Method
@@ -19,6 +20,15 @@ class Question:
             answer.append(random.SystemRandom().choice(string.ascii_letters))
         return ''.join(answer)
 
+    # createSpace Method
+    # * 1개 이상, 단어의 최대 길이 미만의 공백 생성
+    # * 문자열 반환
+    def __createSpace(self):
+        answer = []
+        for _ in range(random.randrange(1, self.__MAX_SPACE_LENGTH)):
+            answer.append(' ')
+        return ''.join(answer)
+
     # createString Method
     # * 1개 이상, 문자열의 최대 길이 미만의 문자열 생성
     # * 문자열 반환
@@ -26,4 +36,5 @@ class Question:
         answer = []
         for _ in range(random.randrange(1, self.__MAX_STRING_LENGTH)):
             answer.append(self.__createWord())
-        return ' '.join(answer)
+            answer.append(self.__createSpace())
+        return ''.join(answer)
